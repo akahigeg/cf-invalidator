e5156e07f4c4e30e0ecc968e20d74574478587f8
akahigeg <akahigeg@gmail.com>
2019-04-20 22:09:19 +0900

    ヘッダとフッタを追加

---
+++ src/components/SiteFooter.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const SiteFooter: React.SFC<{}> = ({}) => {
+  return (
+    <div className="site-footer">
+      <div>(c)brassworks</div>
+    </div>
+  )
+}
+
+export default SiteFooter
---
+++ src/components/SiteHeader.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const SiteHeader: React.SFC<{}> = ({}) => {
+  return (
+    <div className="site-header">
+      <h1>CloudFront Invalidator</h1>
+    </div>
+  )
+}
+
+export default SiteHeader
