41611936bddfe4cb74e532ce207b26f0960aaacb
akahigeg <akahigeg@gmail.com>
2019-04-20 22:50:45 +0900

    フォームをざっくり作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,84 @@
-import * as React from 'react';
-import './App.css';
-import SiteHeader from './components/SiteHeader';
-import SiteFooter from './components/SiteFooter';
+import * as React from "react";
+import "./App.css";
+import SiteHeader from "./components/SiteHeader";
+import SiteFooter from "./components/SiteFooter";
 
-class App extends React.Component {
+import { Form, Button, Container } from "semantic-ui-react";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+interface AppState {
+  distribution_id: string;
+  access_key: string;
+  secret_key: string;
+  path: string;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      distribution_id: cookies.get("distribution_id") || "",
+      access_key: cookies.get("access_key") || "",
+      secret_key: cookies.get("secret_key") || "",
+      path: cookies.get("path") || ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange = (event: any) => {
+    this.setState({ [event.target.name]: event.target.value } as AppState);
+    cookies.set(event.target.name, event.target.value, { path: "/" });
+  };
+
+  handleSubmit = (event: any) => {
+    // use aws-sdk
+  };
+
   public render() {
     return (
-      <div className="App">
-      <SiteHeader />
-        <h1 className="App-title">Welcome to React</h1>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      <SiteFooter />
+      <div>
+        <SiteHeader />
+        <Container className="site-content">
+          <Form className="cf-invalidator">
+            <Form.Input
+              name="distribution_id"
+              label="Distribution ID"
+              type="text"
+              onChange={this.handleInputChange}
+              placeholder="Distribution ID"
+            />
+            <Form.Input
+              name="access_key"
+              label="Access Key"
+              type="text"
+              onChange={this.handleInputChange}
+              placeholder="Access Key"
+            />
+            <Form.Input
+              name="secret_key"
+              label="Secret Key"
+              type="text"
+              onChange={this.handleInputChange}
+              placeholder="Secret Key"
+            />
+            <Form.Input
+              name="path"
+              label="Path"
+              type="text"
+              onChange={this.handleInputChange}
+              placeholder="Path"
+            />
+            <div className="button-area">
+              <Button type="submit" onClick={this.handleSubmit}>
+                キャッシュを削除
+              </Button>
+            </div>
+          </Form>
+        </Container>
+        <SiteFooter />
       </div>
     );
   }
